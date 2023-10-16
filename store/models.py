5db450cdd43d2ac135df3f616c60c792b21e6d8c
chinnnawat <chin3896@gmail.com>
2023-10-16 14:54:16 +0700

    review and comments completed

--- store/models.py
+++ store/models.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from accounts.models import Account
 
 from category.models import Category
-from django.db.models import Avg
+from django.db.models import Avg,Count
 
 # Create your models here.
 
@@ -32,6 +32,13 @@ class Product(models.Model):
         if reviews['average'] is not None:
             avg = float(reviews['average'])
         return avg
+    
+    def countReview(self):
+        reviews = ReviewRating.objects.filter(product = self, status = True).aggregate(count = Count('id'))
+        count = 0
+        if reviews['count'] is not None:
+            count = int(reviews['count'])
+        return count
     
 
 class VariationManager(models.Manager):
